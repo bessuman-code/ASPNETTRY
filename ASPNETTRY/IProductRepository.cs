34f89bcdf00b3a9e9a36f73e8de39af241ed536a
U-DESKTOP-1TUJ31R\iisus <comp.structuralbiology@gmail.com>
2020-07-29 17:41:28 -0400

    first commit

---
+++ ASPNETTRY/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using ASPNETTRY.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETTRY
+{
+    public interface IProductRepository
+    {
+
+
+        public IEnumerable<Product> GetAllProducts();
+
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+
+    }
+}
